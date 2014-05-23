b7540d3e9850fdf7b19963494a51ba923097393d
santo1180 <santo1180@gmail.com>
2014-05-23 16:56:56 -0400

    adding all the files

---
+++ TestCharts/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestCharts.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult SaveImage(string imageData)
+        {
+
+            string fileNameWitPath = ConfigurationManager.AppSettings["ImageLocation"] + DateTime.Now.ToString().Replace("/", "-").
+                Replace(" ", "- ").Replace(":", "") + ".png";
+
+            using (FileStream fs = new FileStream(fileNameWitPath, FileMode.Create))
+            {
+                using (BinaryWriter bw = new BinaryWriter(fs))
+                {
+                    byte[] data = Convert.FromBase64String(imageData);
+                    bw.Write(data);
+                    bw.Close();
+                }
+            }
+
+            return new JsonResult() { Data = true };
+        }
+    }
+}
